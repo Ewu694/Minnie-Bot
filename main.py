271da92acf4f7c10f0311732538913f40e66eeb7
Eric Wu <ericwu694@gmail.com>
2024-10-07 00:38:10 -0400

    Update main.py
    Bot Setup
    - Bot permission setup with intents and client

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
-#STEP 0: Load our token from .env
+# Load our token from .env
 load_dotenv()
 DISCORD_TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-print(DISCORD_TOKEN)
+
+# Bot Setup
+intents: Intents = Intents.default()
+intents.message_content = True
+# Client Setup for bot 
+client: Client = Client(intents = intents)
