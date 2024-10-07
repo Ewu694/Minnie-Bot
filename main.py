243270bb433457b627790a9033b38863557845f3
Eric Wu <ericwu694@gmail.com>
2024-10-07 15:10:44 -0400

    Botstartup and Sample Responses Added
    o Sample Response Logic implemented
    - We don't really need responses for this task bot as it will just be returning based on command called or whatever the user does
    
    o Bot Startup implemented

--- main.py
+++ main.py
@@ -11,5 +11,24 @@ DISCORD_TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 # Bot Setup
 intents: Intents = Intents.default()
 intents.message_content = True
-# Client Setup for bot 
 client: Client = Client(intents = intents)
+
+# Message Functionality
+async def sendMessage(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('Message is empty -> Intents were not enabled')
+        return
+    if is_private := user_message[0] == '?': # ? indicates this is a private message, so this usermessage to be sent is sliced from 1 onwards and stored in is_private and sent to the user after the response is generated
+        user_message = user_message[1:]
+    
+    try:
+        # if message is private, send the response to the user, else send the response to the channel
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response) 
+    except Exception as error:
+        print(f'Error: {error}')
+
+# Bot Handling
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
--- responses.py
+++ responses.py
@@ -1,2 +1,11 @@
+from random import choice, randInt
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('Not Implemented Yet')
+    lowered: str = user_input.lower() 
+
+    if lowered == '':
+        return 'Hi? :('
+    elif lowered == 'hi' or 'hello':
+        return 'Howdy! :3'
+    else:
+        return randInt(1, 100)
